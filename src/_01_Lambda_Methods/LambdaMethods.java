6f265f9aa33cbb7ef3b44a9fe4ef1069baadc746
root <root@f0522f633e68>
2020-05-16 22:57:06 +0000

    LambdaMethods, CreatingStreams, StreamSorter(IP)

--- src/_01_Lambda_Methods/LambdaMethods.java
+++ src/_01_Lambda_Methods/LambdaMethods.java
@@ -6,33 +6,53 @@ public class LambdaMethods {
 	
 		// Here is an example of calling the printCustomMessage method with a lambda.
 		// This prints the passed in String 10 times.
-		//printCustomMessage((s)->{
-			//for(int i = 0; i < 10; i++) {
-				//System.out.println(s);
-			//}
-		//}, "repeat");
+		printCustomMessage((s)->{
+			for(int i = 0; i < 10; i++) {
+				System.out.println(s);
+			}
+		}, "horse");
 		
 		//2. Call the printCustonMessage method using a lambda so that the String prints backwards.
-		String ss;
-		printCustomMessage((a) ->{
+		printCustomMessage((a)->{
 			String h = "";
-			char[] b=a.toCharArray();
-			for(int i = b.length; i < 0; i--) {
-			h = h+b[i];	
-			System.out.println(a);
+			char[] b = a.toCharArray();
+			for(int i = b.length-1; i >=0; i--) {
+				h = h+b[i];
 			}
-			a = h;
-			System.out.println(a);
+			System.out.println(h);
 		}, "horse");
 		//3. Call the printCustonMessage method using a lambda so that the String prints with a mix between upper an lower case characters.
 		printCustomMessage((s) ->{
-		char[]b =s.toCharArray();
-		
+		String r = "";
+		String bean =s.toLowerCase();
+		String omg = s.toUpperCase();
+		for(int i = 0; i < bean.length(); i++) {
+			if(i%2 == 0) {
+			r = r +	bean.charAt(i);
+			}else {
+			r = r+omg.charAt(i);
+			}
+		}
+		System.out.println(r);
 		}, "horse");
 		//4. Call the printCustonMessage method using a lambda so that the String prints with a period in between each character.
-		
+		printCustomMessage((f)->{
+		String b = "";
+		for(int i = 0; i < f.length(); i++) {
+			b = b+ f.charAt(i) + ".";
+		}
+		System.out.println(b);
+		}, "horse");
 		//5. Call the printCustonMessage method using a lambda so that the String prints without any vowels.
-	
+		printCustomMessage((f) -> {
+		String w = "";
+		for(int i = 0; i < f.length(); i++) {
+		if(f.charAt(i) != 'a' && f.charAt(i) != 'e' && f.charAt(i) != 'i' && f.charAt(i) != 'o' && f.charAt(i) != 'u') {
+		w = w+f.charAt(i);
+		}
+		}
+		System.out.println(w);
+		}, "sequoia");
 	}
 	
 	public static void printCustomMessage(SpecialPrinter sp, String value) {
