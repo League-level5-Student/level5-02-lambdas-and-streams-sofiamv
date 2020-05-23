5b7a197df111953bba388124178f194840a48962
root <root@9708ed0eb8b9>
2020-05-23 22:56:29 +0000

    StreamSorter(IP)

--- src/_02_Creating_Streams/CreatingStreams.java
+++ src/_02_Creating_Streams/CreatingStreams.java
@@ -2,6 +2,7 @@ package _02_Creating_Streams;
 
 import java.util.ArrayDeque;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Stack;
 import java.util.stream.Stream;
@@ -10,23 +11,24 @@ public class CreatingStreams {
 	public static void main(String[] args) {
 		//1. Create a stream out of the following collections.
 		String[] strArr = {"one", "two", "three", "four"};
-		
+		Arrays.stream(strArr);
 		ArrayList<Integer> intList = new ArrayList<Integer>();
 		intList.add(1);
 		intList.add(2);
 		intList.add(3);
 		intList.add(4);
-		
+		intList.stream();
 		Stack<Random> randStack = new Stack<Random>();
 		randStack.push(new Random());
 		randStack.push(new Random());
 		randStack.push(new Random());
 		randStack.push(new Random());
-		
+		randStack.stream();
 		ArrayDeque<Double> dQueue = new ArrayDeque<Double>();
 		dQueue.push(0.0);
 		dQueue.push(1.0);
 		dQueue.push(2.0);
 		dQueue.push(3.0);
+		dQueue.stream();
 	}
 }
--- src/_03_Sorting_With_Streams/StreamSorter.java
+++ src/_03_Sorting_With_Streams/StreamSorter.java
@@ -13,10 +13,15 @@ public class StreamSorter {
 		}
 		
 		//1. Convert the Integer array to Stream object.
-		
+		Arrays.stream(nums).sorted();
 		//2. Use the sorted method of the stream to sort the numbers.
 		
 		//3. Use the forEach method with a lambda to print all the elements of the sorted Stream.
 		//   They should be in ascending order.
-	}
+		Arrays.stream(nums).sorted().forEach(s -> {
+			for(int i = 0; i < nums.length; i++) {
+			System.out.println(Arrays.stream(nums).sorted().toString());
+			}
+		});
+}
 }
